2fc75da15eeacedfd68d37cf0dfd9f2433ed7bc2
atsuPueri <yamahata.atsushi.as01@gmail.com>
2023-02-15 13:54:43 +0900

    requireに必要のない()を削除

--- src/index.php
+++ src/index.php
@@ -1,11 +1,10 @@
 <?php
 
 use App\Container;
-use Util\Util;
 
-require(__DIR__ . '/util/view.php'); // autoloadで読み込めない
-require (__DIR__ . '/vendor/autoload.php');
-$web = require_once(__DIR__ . '/route/web.php');
+require __DIR__ . '/util/view.php'; // autoloadで読み込めない
+require __DIR__ . '/vendor/autoload.php';
+$web = require_once __DIR__ . '/route/web.php';
 
 $container = new Container();
 
