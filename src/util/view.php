e96720333e8d6c17e87354312e8c1ed3c38e1092
atsuPueri <yamahata.atsushi.as01@gmail.com>
2023-02-15 13:57:21 +0900

    view関数に表示の際に値を持たせられるように変更

--- src/util/view.php
+++ src/util/view.php
@@ -5,15 +5,19 @@ namespace Util;
 /**
  * @param string $file_path
  */
-function view(string $file_path)
+function view(string $file_path, array $aso_array = [])
 {
     $path = require(__DIR__ . '/../settings/path.php');
     $view_path = $path['view_path'];
-    
+
+    unset($path);
     $open_path = $view_path . $file_path;
 
-    $fp = fopen($open_path, 'r');
-    while (! feof($fp)) {
-        echo fgets($fp, 4);
+    unset($view_path, $file_path);
+
+    foreach ($aso_array as $key => $val) {
+        $$key = $val;
     }
+
+    require $open_path;
 }
