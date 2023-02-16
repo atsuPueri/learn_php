2b03f23f5098aede7fecb5e7c77c78701146f0ae
atsuPueri <yamahata.atsushi.as01@gmail.com>
2023-02-16 19:51:49 +0900

    ファイル書き込み関数追加

--- src/controller/Controller.php
+++ src/controller/Controller.php
@@ -4,5 +4,27 @@ namespace Controller;
 
 class Controller
 {
-    
-}
+    /**
+     * 保存ファイルのパス
+     * @param string $file_path フルパスを書く
+     * @param string $content 書き込む内容
+     */
+    protected function file_put_contents(string $file_path, string $content)
+    {
+
+        $explode = explode("/", $file_path);
+        unset($explode[count($explode) - 1]);
+
+        $check_path = "";
+        foreach ($explode as $val) {
+            $check_path .= $val . "/";
+
+            echo "<BR>", __DIR__ . "/" . $check_path, "<BR>";
+            if (!file_exists(__DIR__ . "/" . $check_path)) {
+                mkdir($check_path);
+            }
+        }
+
+        file_put_contents(__DIR__ . $file_path, $content);
+    }
+
