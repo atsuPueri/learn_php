25428e377324ba5be1230ce339a6d537a7415a7a
atsuPueri <yamahata.atsushi.as01@gmail.com>
2023-02-17 15:01:02 +0900

    CSS追加

--- src/route/web.php
+++ src/route/web.php
@@ -6,9 +6,6 @@ use Controller\Learn;
 $request_path = require(__DIR__ . '/route.php');
 $method = $_SERVER['REQUEST_METHOD'];
 
-var_dump($request_path);
-echo "<br>file:" . __FILE__;
-
 return match ([$request_path, $method]) {
     ['/',      'GET']            => [Top::class, 'show'],
     ['/learn', 'GET']            => [Learn::class, 'show'],
