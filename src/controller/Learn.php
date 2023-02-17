cad832a5582be6446fbaab7ecc09694c12edaba8
atsuPueri <yamahata.atsushi.as01@gmail.com>
2023-02-17 14:59:20 +0900

    送られてきたコードのエラー取得を追加

--- src/controller/Learn.php
+++ src/controller/Learn.php
@@ -22,32 +22,41 @@ class Learn extends Controller
             'title'      => $title,
             'message'    => $message,
             'dir_info'   => [
-                "TEST" => [
-                    "TEST" => [
-                        "TEST" => [
-                            "A.php" => ""
-                        ]
-                    ]
-                ],
                 'index.php' => "<?php\necho 'hello world';"
             ],
         ]);
     }
 
     public function php_basic_exec() {
-        $arr = require __DIR__ . '/../app/learn/message/php_basic.php';
-        $title   = $arr['title'];
-        $message = $arr['message'];
         $sample  = [
             __DIR__ . '/../app/learn/sample/php_basic.php',
         ];
+        
+        $file_info_json = $_POST['files_info'];
+        $now_file       = $_POST['now_file'];
+        $file_info_array = json_decode($file_info_json, true);
+        
+        foreach ($file_info_array as $file_info) {
+            $this->file_put_contents(__DIR__ . "/../public/storage" . $file_info['file_path'], $file_info['content']);
+        }
 
-        view('learn.html', [
-            'title'      => $title,
-            'message'    => $message,
-            'dir_info'   => [
-
-            ],
-        ]);
+        // エラー取得
+        // 実際に出力はさせない為に
+        ob_start();
+        $error = [];
+        try {
+            require __DIR__ . "/../public/storage" . $now_file;
+        } catch (\ParseError $e) {
+            $error['line']    = $e->getLine();
+            $error['message'] = $e->getMessage();
+        } catch (\Error $e) {
+            $error['line']    = $e->getLine();
+            $error['message'] = $e->getMessage();
+        } catch (\Exception $e) {
+            $error['line']    = $e->getLine();
+            $error['message'] = $e->getMessage();
+        }
+        ob_clean();
+        echo json_encode($error);
     }
 }
