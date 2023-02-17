16f6b22aba107a5c8aaa6086113789dfd29034a1
atsuPueri <yamahata.atsushi.as01@gmail.com>
2023-02-17 15:03:38 +0900

    PHPとの通信を追加

--- src/public/js/editor.js
+++ src/public/js/editor.js
@@ -44,7 +44,64 @@ window.addEventListener('load', () => {
     }
 });
 
-// シンタックスハイライトを実行させる
+// PHPとの通信関係
+window.addEventListener('load', () => {
+    const play               = document.getElementById('play');
+    const play_button        = play.getElementsByTagName('button')[0];
+    const file_HTMLColection = document.getElementsByClassName('file');
+
+    play_button.addEventListener('click', () => {
+        
+        const input_code = document.getElementById('input_code');
+        writeFile();
+        const files_info = [];
+        for (const file_element of file_HTMLColection) {
+            const file_path = get_file_path(file_element) + '/' + file_element.textContent;
+            files_info.push({
+                file_path: file_path,
+                content:   file_element.dataset.content,
+            });
+        }
+
+        const send_data = new FormData();
+        send_data.append('files_info', JSON.stringify(files_info));
+        send_data.append('now_file', input_code.dataset.now_file);
+
+
+        fetch(window.location.href, {
+            method: 'POST',
+            body:   send_data,
+        })
+        .then(r => r.text())
+        .then(r => {
+            // 実行後にsrcに結果を反映
+            const result_code = document.getElementById('result_code');
+            result_code.src = window.location.href + '/../../public/storage' + input_code.dataset.now_file;
+
+            const error = JSON.parse(r);
+            const error_element = document.getElementById('error');
+
+            error_element.addEventListener('click', e => {
+                e.target.style.display = 'none';
+            });
+            
+            if (error.line !== undefined) {
+                error_element.style.display = 'block';
+                error_element.style.top     = (error.line * 16) + 'px';
+            } else {
+                error_element.style.display = 'none';
+            }
+        });
+    });
+});
+
+// --------------------------------------------------------
+
+/**
+ * シンタックスハイライトを実行させる
+ * @param {HTMLElement} view_code 
+ * @param {HTMLFormElement} editor_form 
+ */
 function loadEditor(view_code, editor_form) {
     const data = new FormData(editor_form);
     view_code.textContent = data.get('input');
@@ -79,23 +136,33 @@ function get_file_path(file_element) {
 function open_file(file) {
 
     const input_code = document.getElementById('input_code');
+    
+    // 現在のinputの内容を保存
+    writeFile();
+    
+    // inputを選択したものに切り替え
+    input_code.value = file.dataset.content;
+    input_code.dataset.now_file = get_file_path(file) + '/' + file.textContent;
+    loadEditor(view_code, editor_form);
+}
+
+/**
+ * 現在のinputの内容を保存
+ */
+function writeFile() {
+    const input_code         = document.getElementById('input_code');
     const file_HTMLColection = document.getElementsByClassName('file');
     
     // 現在のinputの内容を保存
-    const old_nowFile = input_code.dataset.nowFile;
+    const old_now_file = input_code.dataset.now_file;
     // 現在編集中のファイルを探索
     for (const file_element of file_HTMLColection) {
 
         const file_path = get_file_path(file_element) + '/' + file_element.textContent;
         
-        if (old_nowFile === file_path) {
+        if (old_now_file === file_path) {
             file_element.dataset.content = input_code.value;
             break;
         }
     }
-    
-    // inputを選択したものに切り替え
-    input_code.value = file.dataset.content;
-    input_code.dataset.nowFile = get_file_path(file) + '/' + file.textContent;
-    loadEditor(view_code, editor_form);
 }
