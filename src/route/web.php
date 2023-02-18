8a2616a99bef9b20fe342cb373df830de3eba740
atsuPueri <yamahata.atsushi.as01@gmail.com>
2023-02-19 00:16:02 +0900

    phpの基礎学習ページを追加

--- src/route/web.php
+++ src/route/web.php
@@ -9,6 +9,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 return match ([$request_path, $method]) {
     ['/',      'GET']            => [Top::class, 'show'],
     ['/learn', 'GET']            => [Learn::class, 'show'],
+    
     ['/learn/php_basic', 'GET']  => [Learn::class, 'php_basic'],
     ['/learn/php_basic', 'POST'] => [Learn::class, 'php_basic_exec'],
 
